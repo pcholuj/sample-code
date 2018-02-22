a7bcc0706b6e6e92e3b43e53342ea25958211906
pcholuj <piotr.choluj@filestack.com>
2018-02-22 11:15:57 +0100

    update

--- examples/demo/demo.js
+++ examples/demo/demo.js
@@ -1,14 +1,15 @@
-window.onload = () => {
-    const apikey = 'abc';
-    const client = filestack.init(apikey);
-    client.pick({
-            maxFiles: 20,
-            uploadInBackground: false,
-            onOpen: () => console.log('opened!'),
-        })
-        .then((res) => {
-            console.log(res.filesUploaded)
-            console.log(res.filesFailed)
-        });
+window.addEvent('domready', function () {
+        const apikey = 'abc';
+        const client = filestack.init(apikey);
+        client.pick({
+                maxFiles: 20,
+                uploadInBackground: false,
+                onOpen: () => console.log('opened!'),
+            })
+            .then((res) => {
+                console.log(res.filesUploaded)
+                console.log(res.filesFailed)
+            });
 
+    }
 }
